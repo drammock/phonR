20f50e100de0abbeeb137645a82692cdfc9568ef
drammock <dan.mccloy@gmail.com>
2012-08-11 16:04:15 -0700

    bugfix: changed log, mel, and erb calculations to base-10 instead of base-e

--- normalizeVowels.r
+++ normalizeVowels.r
@@ -6,17 +6,19 @@ normalizeVowels <- function(f0=NULL, f1=NULL, f2=NULL, f3=NULL, method, grouping
   # Adank, P., Smits, R., & van Hout, R. (2004). A comparison of vowel normalization procedures for language variation research. Journal of the Acoustical Society of America, 116, 3099. doi:10.1121/1.1795335.
   # Watt, D., & Fabricius, A. H. (2002). Evaluation of a technique for improving the mapping of multiple speakers’ vowel spaces in the F1 ~ F2 plane. (D. Nelson, Ed.) Leeds Working Papers in Linguistics and Phonetics, 9, 159–173.
   #
-  # bark    <- 26.81*Hz/(1960+Hz)-0.53					        # Traunmüller 1990; Zwicker & Terhardt 1980
-  # mel     <- 2595*log(1+Hz/700)							          # Stevens & Volkmann 1940
-  # erb     <- 21.4*log(1+Hz*0.00437)		        				# Glasberg & Moore 1990
-  # zscore  <- (Hz-grandMeanForTalker)/stDevForTalker	  # Lobanov 1971
-  # nearey1 <- log(Hz) - mean(log(Hz))					        # Nearey 1978
-  # nearey2 <- log(Hz) - sum(mean(log(f0)),mean(log(f1)),mean(log(f2)),mean(log(f3)))
-  # s-centroid                                          # see Watt & Fabricius 2002 for calculations.
+  # bark        <- 26.81*Hz/(1960+Hz)-0.53					        # Traunmüller 1990; Zwicker & Terhardt 1980
+  # mel         <- 2595*log(1+Hz/700)							          # Stevens & Volkmann 1940
+  # erb         <- 21.4*log(1+Hz*0.00437)		        				# Glasberg & Moore 1990
+  # zscore      <- (Hz-grandMeanForTalker)/stDevForTalker	  # Lobanov 1971
+  # nearey1     <- log(Hz) - mean(log(Hz))					        # Nearey 1977
+  # nearey2     <- log(Hz) - sum(mean(log(f0)),mean(log(f1)),mean(log(f2)),mean(log(f3)))
+  # s-centroid  <- Hz_n/centroid_n                          # see Watt & Fabricius 2002 for full explanation
   #
-  # VERSION 0.4 (2012 07 21)
+  # VERSION 0.5 (2012 08 11)
   #
   # CHANGELOG
+  # VERSION 0.5: fixed log, mel, and erb calculations to use base-10 instead of base-e.
+  #
   # VERSION 0.4: added S-centroid normalization method.
   #
   # VERSION 0.3: minor improvement to error handling.
@@ -25,7 +27,7 @@ normalizeVowels <- function(f0=NULL, f1=NULL, f2=NULL, f3=NULL, method, grouping
   #
   # AUTHOR: DANIEL MCCLOY (drmccloy@uw.edu)
   # LICENSED UNDER THE GNU GENERAL PUBLIC LICENSE v3.0: http://www.gnu.org/licenses/gpl.html
-  # DEVELOPMENT OF THIS SCRIPT WAS FUNDED IN PART BY THE NATIONAL INSTITUTES OF HEALTH, GRANT # 10186254 TO PAMELA SOUZA
+  # DEVELOPMENT OF THIS SCRIPT WAS FUNDED IN PART BY THE NATIONAL INSTITUTES OF HEALTH, GRANT # R01DC006014 TO PAMELA SOUZA
 
   m <- tolower(method)
 
@@ -52,20 +54,22 @@ normalizeVowels <- function(f0=NULL, f1=NULL, f2=NULL, f3=NULL, method, grouping
     f <- cbind(as.vector(f1),as.vector(f2))
   }
 
+  if (is.null(grouping.factor)) { grouping.factor <- 'noGroups' }
+
   if (m=='bark') {
     fn <- 26.81*f/(1960+f)-0.53
 
   } else if (m=='log') {
-    fn <- log(f)
+    fn <- log10(f)
 
   } else if (m=='mel') {
-    fn <- 2595*log(1+f/700)
+    fn <- 2595*log10(1+f/700)
 
   } else if (m=='erb') {
-    fn <- 21.4*log(0.00437*f+1)
+    fn <- 21.4*log10(0.00437*f+1)
 
   } else if (m %in% c('s','scentroid','wattfabricius')) {
-    if (is.null(grouping.factor)) { grouping.factor <- 'noGroups' }
+#    if (is.null(grouping.factor)) { grouping.factor <- 'noGroups' }
     subsets <- by(f, list(grouping.factor,vowel), subset) # 2D list (group x vowel) of lists (f1,f2)
     means.list <- matrix(lapply(subsets, function(x){apply(x,2,mean)}), ncol=ncol(subsets), dimnames=dimnames(subsets))
     f1maxima <- apply(means.list,1,function(x){max(matrix(unlist(x),ncol=2,byrow=TRUE)[,1])})
@@ -90,7 +94,8 @@ normalizeVowels <- function(f0=NULL, f1=NULL, f2=NULL, f3=NULL, method, grouping
     centroid <- cbind(apply(as.matrix(grouping.factor),1,function(x){f1.centroid[x]}), apply(as.matrix(grouping.factor),1,function(x){f2.centroid[x]}))
     fn <- f/centroid
 
-  } else if (is.null(grouping.factor)) {
+#  } else if (is.null(grouping.factor)) {
+  } else if (grouping.factor[1]=='noGroups') {
     if (m %in% c('lobanov','ztransform','zscore','z')) {
       fn <- (f - rep(apply(f,2,mean),each=nrow(f))) / rep(apply(f,2,sd),each=nrow(f))
       
