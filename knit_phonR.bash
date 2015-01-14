3094fd8dae47ba898e25636694d3a546596a918b
Daniel McCloy <dan.mccloy@gmail.com>
2015-01-14 15:58:22 +0800

    html build fully working

--- knit_phonR.bash
+++ knit_phonR.bash
@@ -1,13 +1,17 @@
 # /bin/bash
-
-#Rscript -e "knitr::knit2html('phonR.Rmd', options=c('use_xhtml', 'smartypants', 'mathjax', 'highlight_code'))"
-Rscript -e "knitr::knit('phonR.Rmd')"
+Rscript -e "knitr::knit('phonR.Rmd')"   # phonR.Rmd > phonR.md
 cd images
 for i in *.pdf; do 
 	inkscape -f "$i" -d 72 -b "white" -e "$(basename $i .pdf).png"
 done
 cd ..
-python vignette_postprocessing.py
-#pandoc -s phonR-png.md -c phonR.css -o index.html 
-#pandoc -s --latex-engine=xelatex --template=vignette_template.tex phonR.md -o phonR.pdf
- 
+python vignette_postprocessing.py       # phonR.md > index.md
+Rscript -e "knitr::pandoc('index.md')"  # index.md > phonR.html
+#python vignette_mathjax_resizer.py     # phonR.html > index.html
+mv phonR.html index.html
+
+# cleanup
+rm phonR.html
+rm *.pyc
+rm index.md
+rm phonR.md
